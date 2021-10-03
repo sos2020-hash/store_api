71aa15944c943724a44a51ece8277e4e4be5c90a
sos2020-hash <qweiop525@gmail.com>
2021-10-03 22:04:57 +0800

    first commit

---
+++ controllers/products.js
@@ -0,0 +1,24 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    name: "vase table",
+  });
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+  const { featured } = req.query;
+
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = {
+  getAllProductsStatic,
+  getAllProducts,
+};
